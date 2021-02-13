ad2d983e649bfbab45de5154d5309cffebe75621
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-13 19:06:21 +0200

    Add Prepare Data

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas_datareader as web
 import datetime as dt
 
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.model import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM
+# from tensorflow.keras.model import Sequential
+# from tensorflow.keras.layers import Dense, Dropout, LSTM
 
 # Load Data
 company = "FB"
@@ -16,3 +16,19 @@ end = dt.datetime(2021,1,1)
 
 data = web.DataReader(company, 'yahoo', start, end)
 
+
+# Prepare Data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
