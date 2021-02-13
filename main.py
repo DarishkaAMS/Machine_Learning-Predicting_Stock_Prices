45c2680a85e20416b61781ccb07d3f5e0b9ddf8d
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-13 18:58:02 +0200

    Add Load Data

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ import datetime as dt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.model import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+# Load Data
+company = "FB"
+
+start = dt.datetime(2012,2,2)
+end = dt.datetime(2021,1,1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
